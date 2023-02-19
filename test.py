18cf28a5783d5ddf482b30960c7a427356e54014
root <root@localhost.localdomain>
2023-02-19 01:31:14 +0000

    1

---
+++ test.py
@@ -0,0 +1,18 @@
+import socket
+
+
+# http
+def http():
+    a = socket.socket()
+    a.bind(("0.0.0.0", 0))
+    a.connect(("192.168.1.1", 80))
+    a.send(b"GET / HTTP/1.0\r\nHost: 192.168.1.1\r\n\r\n")
+    z = a.recv(1024)
+    data = b""
+    while z:
+        data = data+z
+        z = a.recv(1024)
+    print(data)
+
+
+http()
