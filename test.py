283a7862fce8d6346dca541f07bc37e4530aade3
root <root@localhost.localdomain>
2023-02-21 17:40:17 +0000

    y

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ def http():
     a.bind(("0.0.0.0", 0))
     a.connect(("192.168.1.1", 80))
     a.send(b"GET / HTTP/1.0\r\nHost: 192.168.1.1\r\n\r\n")
-    z = a.recv(1024)
+    _ = a.recv(1024)
     data = b""
     while z:
-        data = data+z
-        z = a.recv(1024)
+        data = data + _
+        _ = a.recv(1024)
     print(data)
 
 
